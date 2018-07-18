82977e6df931bf05ea64948b56cd65e3c2471b67
Wayne Foux <Wayne_Foux@cable.comcast.com>
2018-07-18 14:08:18 -0400

    Refactoring of Routes
    Split Routes into Routes and Router

--- Logger.go
+++ Logger.go
@@ -7,17 +7,18 @@ import (
 )
 
 func Logger(inner http.Handler, name string) http.Handler {
-	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-		start := time.Now()
+	return http.HandlerFunc(
+		func(writer http.ResponseWriter, request *http.Request) {
+			start := time.Now()
 
-		inner.ServeHTTP(writer, request)
+			inner.ServeHTTP(writer, request)
 
-		log.Printf(
-			"%s\t%s\t%s\t%s",
-			request.Method,
-			request.RequestURI,
-			name,
-			time.Since(start),
-		)
-	})
+			log.Printf(
+				"%s\t%s\t%s\t%s",
+				request.Method,
+				request.RequestURI,
+				name,
+				time.Since(start),
+			)
+		})
 }
